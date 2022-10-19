ba79e084397be671f6317f070a4a343e27a3dcf1
Pavel_Vatsek <pvatsek@gmail.com>
2022-10-19 21:37:08 +0300

    add Tasks

---
+++ Task13/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Write("Введите число: ");
+int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+if (inputNumber < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+    }
+else
+{
+    while (inputNumber > 1000)
+    {
+    inputNumber = inputNumber / 10;
+    }
+int result = inputNumber % 10;
+Console.WriteLine(result);
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет,
+ является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Write("Введите номер дня недели: ");
+int inputDay = Convert.ToInt32(Console.ReadLine());
+
+if (inputDay > 0 && inputDay < 6)
+{
+    Console.WriteLine("Это рабочий день");
+}
+else if (inputDay > 5 && inputDay < 8)
+{
+    Console.WriteLine("Это выходной");
+}
+else
+{
+        Console.WriteLine("Такого дня недели не существует");
+}
