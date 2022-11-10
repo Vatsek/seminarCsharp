c93a27fcb02a4c574bec813123cb7f33bcc313bb
Pavel_Vatsek <pvatsek@gmail.com>
2022-11-10 23:11:26 +0300

    add tasks 24, 26,28, 29

---
+++ Task26/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while(number > 9)
+{
+    number /= 10;
+    count++;
+}
+Console.WriteLine(count);
---
+++ Task29/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами 
+в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+
+// int[] numbers = new int[8];
+// Random rnd = new Random();
+// int i = 0;
+// for (; i < numbers.Length-1; i++)
+//     {   
+//         numbers[i] = rnd.Next(0,2);
+//         Console.Write(numbers[i] + ", ");
+//     }
+// Console.Write(numbers[i]);
+
+
+int size = 8;
+int[] numbers = new int[size];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(0, 2);
+    Console.Write(numbers[i] + " ");
+}
